55041ed13ef06ac9099557fe9865084953afb65e
Soe Thazin Htet <soethazinhtet@my.jcu.edu>
2026-05-30 14:33:50 +0800

    Add docstrings

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,16 +1,19 @@
 MINIMUM_LENGTH = 5
 
 def main():
+    """Password checking program."""
     password = get_password()
 
     display_asterisks(password)
 
 
 def display_asterisks(password):
+    """Display asterisks based on the length of the password."""
     print("*" * len(password))
 
 
 def get_password() -> str:
+    """Determine if the password is valid."""
     password = input("Enter password: ")
     while len(password) < MINIMUM_LENGTH:
         print("Your password is too short")
