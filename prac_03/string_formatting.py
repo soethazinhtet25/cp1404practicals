557e49115cab5a91581d0f553356aa54d2097622
Soe Thazin Htet <soethazinhtet@my.jcu.edu>
2026-06-10 22:43:23 +0800

    Use for loop with f-string formatting

--- prac_03/string_formatting.py
+++ prac_03/string_formatting.py
@@ -36,11 +36,13 @@ for i, number in enumerate(numbers, 1):
 name = "Gibson L-5 CES"
 year = 1922
 cost = 16035.90
-print(f"{year} {name} for about ${cost:,.0f}")
+print(f"{year} {name} for about ${cost:,.0f}!")
 # 1922 Gibson L-5 CES for about $16,036!
 
 # TODO: Using a for loop with the range function and f-string formatting,
 # produce the following right-aligned output (DO NOT use a list):
+for i in range(11):
+    print(f"2 ^{i:>2} is {2**i:>4}")
 # 2 ^ 0 is    1
 # 2 ^ 1 is    2
 # 2 ^ 2 is    4
