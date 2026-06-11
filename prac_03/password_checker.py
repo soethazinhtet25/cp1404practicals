fb98b4e08760b5971b9b175fa3e863205862934d
Soe Thazin Htet <soethazinhtet@my.jcu.edu>
2026-06-12 00:17:16 +0800

    Add lowercase character validation

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -35,8 +35,8 @@ def is_valid_password(password):
     number_of_digit = 0
     number_of_special = 0
     for character in password:
-        # TODO: count each kind of character (use str methods like isdigit)
-        pass
+        if character.islower():
+            number_of_lower += 1
 
     # TODO: if any of the 'normal' counts are zero, return False
 
