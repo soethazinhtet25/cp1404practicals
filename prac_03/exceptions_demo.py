46ccd56095e8d8486097eb9bac8522b756ee4e16
Soe Thazin Htet <soethazinhtet@my.jcu.edu>
2026-06-11 22:31:54 +0800

    Add if check for division by zero

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -9,8 +9,11 @@ Answer the following questions:
 try:
     numerator = int(input("Enter the numerator: "))
     denominator = int(input("Enter the denominator: "))
-    fraction = numerator / denominator
-    print(fraction)
+    if denominator == 0:
+        print("Cannot divide by zero!")
+    else:
+        fraction = numerator / denominator
+        print(fraction)
 except ValueError:
     print("Numerator and denominator must be valid numbers!")
 except ZeroDivisionError:
