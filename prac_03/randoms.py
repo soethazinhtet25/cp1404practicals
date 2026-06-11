519f1bdef2b2290faf03c913352f84a5f05b37bc
Soe Thazin Htet <soethazinhtet@my.jcu.edu>
2026-06-11 18:45:54 +0800

    Add random number answers and code

---
+++ prac_03/randoms.py
@@ -0,0 +1,19 @@
+import random
+
+# What did you see on line 1?
+# 8
+# Smallest number: 5
+# Largest number: 20
+
+# What did you see on line 2?
+# 5
+# Smallest number: 3
+# Largest number: 9
+# Could line 2 have produced a 4? No
+
+# What did you see on line 3?
+# 3.7426096668416022
+# Smallest number: 2.5
+# Largest number: 5.5
+
+print(random.randint(1,100))
