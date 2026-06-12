25e7dbe9a7b90a56c5e1a56e0107b4c4d383181f
Soe Thazin Htet <soethazinhtet@my.jcu.edu>
2026-06-12 11:17:52 +0800

    Create file size function

--- prac_03/file_size.py
+++ prac_03/file_size.py
@@ -3,3 +3,10 @@ filename = input("filename: ")
 while filename != "":
     print(filename)
     filename = input("filename: ")
+
+def get_file_size(filename):
+    line_count = 0
+    with open(filename, "r") as in_file:
+        for line in in_file:
+            line_count += 1
+    return line_count
