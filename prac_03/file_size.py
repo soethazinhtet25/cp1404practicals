297a597903a3b006e7bbb404a8ab50d4a55969d6
Soe Thazin Htet <soethazinhtet@my.jcu.edu>
2026-06-12 11:25:45 +0800

    Add file not found exception handling

--- prac_03/file_size.py
+++ prac_03/file_size.py
@@ -1,12 +1,15 @@
-filename = input("filename: ")
-
-while filename != "":
-    print(filename)
-    filename = input("filename: ")
-
 def get_file_size(filename):
     line_count = 0
     with open(filename, "r") as in_file:
         for line in in_file:
             line_count += 1
     return line_count
+
+filename = input("filename: ")
+
+while filename != "":
+    try:
+        print(f"{filename} has {get_file_size(filename)} lines.")
+    except FileNotFoundError:
+        print(f"ERROR: {filename} does not exist.")
+    filename = input("filename: ")
