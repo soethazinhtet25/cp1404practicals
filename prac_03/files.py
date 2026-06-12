8e5ebfc779f65bda0ed2b31b71d77a062b081af2
Soe Thazin Htet <soethazinhtet@my.jcu.edu>
2026-06-12 10:34:19 +0800

    Read first two numbers and calculate total

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,11 +1,18 @@
 # 1.
-name = input("Name: ")
-out_file = open("name.txt", "w")
-out_file.write(name)
-out_file.close()
+# name = input("Name: ")
+# out_file = open("name.txt", "w")
+# out_file.write(name)
+# out_file.close()
 
 # 2.
-in_file = open("name.txt", "r")
-text = in_file.read()
-print(f"Hi {text}!")
-in_file.close()
+# in_file = open("name.txt", "r")
+# text = in_file.read()
+# print(f"Hi {text}!")
+# in_file.close()
+
+# 3.
+with open("numbers.txt", "r") as in_file:
+    first_number = int(in_file.readline())
+    second_number = int(in_file.readline())
+
+print(first_number + second_number)
