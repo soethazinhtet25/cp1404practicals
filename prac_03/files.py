06f446360ef58bbcb3bbaedff1a791828088622c
Soe Thazin Htet <soethazinhtet@my.jcu.edu>
2026-06-12 10:27:14 +0800

    Read name from file and print greeting

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,8 +1,8 @@
 # 1.
-# name = input("Name: ")
-# out_file = open("name.txt", "w")
-# out_file.write(name)
-# out_file.close()
+name = input("Name: ")
+out_file = open("name.txt", "w")
+out_file.write(name)
+out_file.close()
 
 # 2.
 in_file = open("name.txt", "r")
